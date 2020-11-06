c363492dd29f7fbcbf8799a361d5cc7e486f3e1c
Urbanproof <urbanproof2@gmail.com>
2020-11-06 16:01:46 +0200

    Sorting options added (#1)
    * Sorting options added
    
    * Fixed incorrect usage of escaping functions
    
    * Added sorting order field
    
    * Fixed sorting values
    
    * Added some documentation

--- README.md
+++ README.md
@@ -1,15 +1,38 @@
 # ACF Filters Field
 
-TODO:
+This is a simple WordPress plugin that adds a a new type of ACF field; filter-field. This doesn't do much by itself, but combined with custom WP_Queries this provides a very flexible way of adding different "collections" into your pages. The main purpose is to create a single layout/block etc. that you can dynamically use in different places. For example, you could show blog posts on one page and news on another without the need of defining multiple field groups.
+
+TLDR; this is sort of ACF interface for WP_Query.
+
+## Requirements
+* WordPress 5.5.0 on newer
+* PHP 7.2 or newer
+* Advanced Custom Fields 5.0 or newer
+* (optional) Composer
+    * You can install this wihtout composer too, but thats on you
+
+## Installation
+1. Use (Composer-Dropin-Installer)[https://github.com/Koodimonni/Composer-Dropin-Installer] to define your installer paths
+2. Add this repository;
+    ```json
+    "repositories": [
+        {
+            "type": "vcs",
+            "url": "https://github.com/Urbanproof/acf-filters.git"
+        },
+    ],
+    ```
+3. And require the package; `composer require urbanproof/acf-filters`
+
+## TODO:
 * This documentation
     * What this does
     * How it looks
     * How to use
     * Example with WP_Query
 * Internationalization
-* Bake into Composer package
-* PHPCS ruleset
-* ACF4 support? Nah...
 * More options:
-    * sorting order
-    * exclusions?
+    * limits
+    * exclusions
+    * search
+    * author
--- classes/Fields/FilterField.php
+++ classes/Fields/FilterField.php
@@ -23,6 +23,60 @@ class FilterField extends \acf_field {
 	public function render_field( array $field )
 	{
 		/**
+		 * Sorting options
+		 */
+		$order_by_fields = array(
+			'none'          => __( 'No ordering', 'acf-filters' ),
+			'ID'            => __( 'Order by post ID', 'acf-filters' ),
+			'author'        => __( 'Order by author', 'acf-filters' ),
+			'title'         => __( 'Order by title.', 'acf-filters' ),
+			'name'          => __( 'Order by post name', 'acf-filters' ),
+			'type'          => __( 'Order by post type', 'acf-filters' ),
+			'date'          => __( 'Order by date', 'acf-filters' ),
+			'modified'      => __( 'Order by last modified date', 'acf-filters' ),
+			'parent'        => __( 'Order by post/page parent id', 'acf-filters' ),
+			'rand'          => __( 'Random order', 'acf-filters' ),
+			'comment_count' => __( 'Order by number of comments', 'acf-filters' ),
+			'menu_order'    => __( 'Order by Page Order', 'acf-filters' ),
+		);
+		?>
+		<fieldset>
+			<legend>
+				<?php esc_html_e( 'Sorting options', 'acf-filters' ); ?>
+			</legend>
+			<label>
+				<?php esc_html_e( 'Order results by:', 'acf-filters' ); ?>
+				<select name="<?php echo esc_attr( $field['name'] ); ?>[orderby]">
+					<?php foreach ( $order_by_fields as $value => $label ) : ?>
+						<option
+							value="<?php echo esc_attr( $value ); ?>"
+							<?php selected( $field['value']['orderby'] === $value ); ?>
+						>
+							<?php echo esc_html( $label ); ?>
+						</option>
+					<?php endforeach; ?>
+				</select>
+			</label>
+			<label>
+				<?php esc_html_e( 'Sorting order:', 'acf-filter' ); ?>
+				<select name="<?php echo esc_attr( $field['name'] ); ?>[order]">
+					<option
+						value="ASC"
+						<?php selected( $field['value']['order'] === $value ); ?>
+					>
+						<?php esc_html_e( 'Ascending', 'acf-filters' ); ?>
+					</option>
+					<option
+						value="DESC"
+						<?php selected( $field['value']['order'] === $value || ! isset( $field['value']['order'] ) ); ?>
+					>
+						<?php esc_html_e( 'Descending', 'acf-filters' ); ?>
+					</option>
+				</select>
+			</label>
+		</fieldset>
+		<?php
+		/**
 		 * Post types
 		 */
 		$post_types = get_post_types( array( 'exclude_from_search' => false ), 'objects' );
